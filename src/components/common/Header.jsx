1892ca5c43c5c59374136321daf72ee7e7b8a83a
vijendersuthar1 <vijendersuthar1234@gmail.com>
2025-12-06 12:30:30 +0530

    dev

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+/** @format */
+
+import Link from "next/link";
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="max-w-[1170px] w-full flex justify-between mx-auto font-[Quicksand]">
+      <Link href="#" className="font-semiBold text-[42px] leading-[63px]">
+        Logo
+      </Link>
+      <div className="flex justify-between">
+        <div className="max-w-[">
 
-export default Header
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
