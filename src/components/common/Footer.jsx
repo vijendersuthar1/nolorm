87b1743fc340a3550ac11a1f56e76cdb0730734f
Poonia <rohtashpoonia2274@gmail.com>
2025-12-13 15:50:36 +0530

    marge

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -12,24 +12,34 @@ import PageBanner from "./PageBanner";
 import Link from "next/link";
 import Image from "next/image";
 
-
 const Footer = () => {
   return (
     <div className="max-w-[1170px] w-full mx-auto px-4 flex mt-[127px] gap-[87px] max-lg:flex-col max-lg:mt-[30px] max-lg:gap-10">
       <div className="max-w-[262px] w-full">
         <Image
-                  src="/images/Logo.png"
-                  alt="logo"
-                  width={122}
-                  height={71}
-                  className="cursor-pointer"
-                />
+          src="/images/Logo.png"
+          alt="logo"
+          width={122}
+          height={71}
+          className="cursor-pointer"
+        />
         <div className="flex gap-2 pt-[38px] cursor-pointer">
-          <Link href='/'> <Facebook/></Link>
-          <Link href='/'><Twitter/></Link>
-          <Link href='/'><Instagram /></Link>
-          <Link href='/'><Youtube /></Link>
-          <Link href='/'><Whatsapp /></Link>
+          <Link href="/">
+            {" "}
+            <Facebook />
+          </Link>
+          <Link href="/">
+            <Twitter />
+          </Link>
+          <Link href="/">
+            <Instagram />
+          </Link>
+          <Link href="/">
+            <Youtube />
+          </Link>
+          <Link href="/">
+            <Whatsapp />
+          </Link>
         </div>
         <div className="pt-[30.5px]">
           {logodata.map((items, index) => (
@@ -45,37 +55,54 @@ const Footer = () => {
       <div>
         <div className="grid grid-cols-3 gap-[126px] max-md:grid-cols-2 max-sm:grid-cols-1  max-md:gap-[50px] cursor-pointer  ">
           {FooterLink.map((items, index) => (
-            <div className="flex flex-col gap-2">
-              <h3 className="text-[18px] text-[#253D4E] font-[poppins] font-semibold">{items.heading}</h3>
+            <div key={index} className="flex flex-col gap-2">
+              <h3 className="text-[18px] text-[#253D4E] font-[poppins] font-semibold">
+                {items.heading}
+              </h3>
               <ul className="text-[14px] text-[#253D4E] font-[open_Sans] font-normal leading-[21px] gap-2.5 flex flex-col   ">
-               {items.links.map((link,i)=>{
-                return(
-                  <Link key={i} href={link.url} className="hover:text-blue-500 ">{link.title}</Link>
-                )
-               })}
+                {items.links.map((link, i) => {
+                  return (
+                    <Link
+                      key={i}
+                      href={link.url}
+                      className="hover:text-blue-500 "
+                    >
+                      {link.title}
+                    </Link>
+                  );
+                })}
               </ul>
             </div>
           ))}
         </div>
         <div className="pt-10">
-          <h2 className="text-[18px] text-[#130017] font-[quicksand] font-semibold ">Product tags</h2>
+          <h2 className="text-[18px] text-[#130017] font-[quicksand] font-semibold ">
+            Product tags
+          </h2>
           <div className="flex gap-4 flex-wrap pt-3">
-            {productTags.map((tag,index)=>(
-              <button key={index} className="text-[12px] text-[#253D4E] font-[quicksand] font-semibold bg-[#F2F0F0] rounded-xl py-1 items-center flex px-[9px]">{tag}</button>
+            {productTags.map((tag, index) => (
+              <button
+                key={index}
+                className="text-[12px] text-[#253D4E] font-[quicksand] font-semibold bg-[#F2F0F0] rounded-xl py-1 items-center flex px-[9px]"
+              >
+                {tag}
+              </button>
             ))}
-            
           </div>
           <div className="flex justify-between items-center mt-[9px] border-t border-t-[#939393] max-md:flex-col max-md max-md:justify-center">
-            <p className="text-[14px] text-[#253D4E] font-[lato] max-md:pt-5 ">Copyright © 2022 <span className="text-[#130017]">ThemeRage</span>. All Right Reserved.</p>
+            <p className="text-[14px] text-[#253D4E] font-[lato] max-md:pt-5 ">
+              Copyright © 2022 <span className="text-[#130017]">ThemeRage</span>
+              . All Right Reserved.
+            </p>
             <div className="flex gap-6 h-28 items-center  ">
-             <div className="flex gap-2.5 ">
-               <Mobile/>
-              <p className="text-[14px] text-[#253D4E]">(684) 555-0102</p>
-             </div>
-             <div className="flex gap-2.5">
-               <Mobile/>
-              <p className="text-[14px] text-[#253D4E]">(684) 555-0102</p>
-             </div>
+              <div className="flex gap-2.5 ">
+                <Mobile />
+                <p className="text-[14px] text-[#253D4E]">(684) 555-0102</p>
+              </div>
+              <div className="flex gap-2.5">
+                <Mobile />
+                <p className="text-[14px] text-[#253D4E]">(684) 555-0102</p>
+              </div>
             </div>
           </div>
         </div>
